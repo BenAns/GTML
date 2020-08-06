f8db9553f69346a17cbabdca210f3376a6f7f3e9
Ben Anscombe <benjamin.anscombe@hotmail.com>
2020-08-06 21:40:02 +0100

    Made a basic CMake project/folder structure for the library

---
+++ include/GTML.hpp
@@ -0,0 +1,6 @@
+#ifndef GTML_HPP
+#define GTML_HPP
+
+// File to include all parts of GTML for use in a project
+
+#endif
---
+++ tests/unit/src/main.cpp
@@ -0,0 +1,8 @@
+#include <GTML.hpp>
+
+// File to call unit tests for GTML
+
+int main()
+{
+	return 0;
+}
